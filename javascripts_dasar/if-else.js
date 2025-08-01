41087bb0c34eac81c0a0cde40b58a72beee6a50f
mathaillah <mathaillah96@gmail.com>
2025-08-01 18:51:31 +0700

    belajar if else

---
+++ javascripts_dasar/if-else.js
@@ -0,0 +1,6 @@
+var angka = prompt("Masukkan angka: ");
+if (angka % 2 === 0) {
+    console.log(angka + " adalah bilangan genap.");
+} else {
+    console.log(angka + " adalah bilangan ganjil.");
+}   
