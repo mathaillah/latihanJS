f485e8bdf6c6ecc41a6722e0adc4be8ac010d4cc
mathaillah <mathaillah96@gmail.com>
2025-09-15 21:54:11 +0700

    Belajar tanggal 15/09/25

---
+++ function.js
@@ -0,0 +1,17 @@
+// function adalah kunci utama pada javascripts yang membuat javascript menjadi bahasa pemrograman yang powerful
+
+// function adalah sebuah blok kode yang dapat digunakan kembali
+// function dapat menerima input (parameter) dan mengembalikan output (return)
+
+// Built-in Function
+// alert() -> menampilkan pesan dalam kotak dialog
+// prompt() -> menampilkan kotak dialog dengan input teks
+// confirm() -> menampilkan kotak dialog dengan pilihan OK dan Cancel
+// console.log() -> menampilkan pesan di konsol browser
+
+// user-defined function
+// function namaFunction(parameter1, parameter2) {
+//     // kode yang akan dijalankan
+//     return hasil; // mengembalikan nilai
+// }    
+
