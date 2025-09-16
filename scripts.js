4529ad7f5cde95b4b06d99ab9a5918b34d4fd811
mathaillah <mathaillah96@gmail.com>
2025-09-16 20:32:15 +0700

    belajar js 16/09/2025

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@ var a = 8;
 var b ;
 var volumeA;
 var volumeB;
+var volumeC;
 var totalVolume;
 
 volumeA = a* a*a;
 volumeB = b* b*b;
+volumeC = c*c*c;
 total = volumeA + volumeB; 
 
 console.log(total);
